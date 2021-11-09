138646120d1c5fab244720cb7b494a60c0f9b26b
Szymon Gackowski <szymon.gackowski@gmail.com>
2021-11-09 21:42:05 +0100

    Added cooperation page

---
+++ resources/views/cooperation.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="navbar-background">
+    </div>
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-12">
+                <h2>Współpraca</h2>
+            </div>
+        </div>
+    </div>
+@endsection
